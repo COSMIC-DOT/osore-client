ca41736eafa2d29e8aef70b356642fcdb64560ec
Kiwon Kim <kiuuon29@gmail.com>
2024-04-08 19:25:23 +0900

    setting: 프로젝트 초기 세팅 (#2)
    * initial commit
    
    * feat: prettier 설정
    
    * feat: eslint 설정
    
    airbnb 스타일 가이드로 설정
    
    * feat: 프리티어 설정 수정
    
    * chore: 불필요한 파일 정리
    
    * docs: 기본 리드미 파일 삭제

---
+++ src/app/layout.tsx
@@ -0,0 +1,17 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: '오소리',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body>{children}</body>
+    </html>
+  );
+}
