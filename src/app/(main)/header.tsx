c88a40571965b0a32cce3000395bacffd2e571b1
kiuuon <k41471804@gmail.com>
2024-05-22 11:13:31 +0900

    feat: 유저 정보 받아와서 프로필 컴포넌트 구현
    axios 객체를 만들었습니다.
    route hanlder를 사용합니다.

--- src/app/(main)/header.tsx
+++ src/app/(main)/header.tsx
@@ -1,21 +1,20 @@
+import { Suspense } from 'react';
 import Image from 'next/image';
 
-export default function Header() {
+import Profile from '@/components/profile';
+
+function Header() {
   return (
     <header className="mx-auto my-[40px] flex w-[95%] items-center justify-between">
       <div className="flex h-[47px] w-[223px] items-center justify-between">
         <Image className="h-[47px] w-[36px]" src="/images/icon.png" alt="아이콘" height={47} width={36} />
         <Image className="h-[32px] w-[174px]" src="/images/logo.png" alt="로고" height={32} width={174} />
       </div>
-      <div className="flex h-[60px] w-[60px] items-center justify-center overflow-hidden rounded-full bg-white">
-        <Image
-          className="h-[60px] w-[60px]"
-          src="https://avatars.githubusercontent.com/u/74997112?v=4"
-          alt="프로필"
-          width={50}
-          height={50}
-        />
-      </div>
+      <Suspense fallback={<div className="h-[60px] w-[60px] rounded-full bg-gray-500" />}>
+        <Profile />
+      </Suspense>
     </header>
   );
 }
+
+export default Header;
