1eae4940cbd96bf9ab6be7b11136f6748a2bd02c
kiuuon <k41471804@gmail.com>
2024-05-24 18:28:40 +0900

    feat: 노트 리스트 페이지 이동 구현

--- src/app/(main)/finder/note-list.tsx
+++ src/app/(main)/finder/note-list.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { use } from 'react';
+import { use, useState } from 'react';
 
 import NoteType from '@/types/note-type';
 import PrevIcon from '@/icons/prev-icon';
@@ -8,6 +8,8 @@ import NextIcon from '@/icons/next-icon';
 import Note from './note';
 
 function NoteList() {
+  const [page, setPage] = useState(0);
+
   const notes = use(
     fetch('/api/notes', {
       method: 'GET',
@@ -16,10 +18,22 @@ function NoteList() {
       .then(({ data }) => data.noteList),
   );
 
+  const handlePrevButton = () => {
+    if (page > 0) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextButton = () => {
+    if (page < notes.length - 1) {
+      setPage(page + 1);
+    }
+  };
+
   return (
     <main className="mx-auto mt-[60px] flex w-[95%] flex-col gap-[58px]">
       <div className="mx-auto flex max-w-[1800px] flex-wrap gap-[20px]">
-        {notes.map((note: NoteType) => (
+        {notes[page].map((note: NoteType) => (
           <Note key={note.id} note={note} />
         ))}
       </div>
@@ -27,14 +41,16 @@ function NoteList() {
         <button
           type="button"
           className="flex h-[32px] w-[32px] items-center justify-center border-[1px]"
+          onClick={handlePrevButton}
           aria-label="이전 노트 리스트"
         >
           <PrevIcon />
         </button>
-        1/3
+        {page + 1}/{notes.length}
         <button
           type="button"
           className="flex h-[32px] w-[32px] items-center justify-center border-[1px]"
+          onClick={handleNextButton}
           aria-label="다음 노트 리스트"
         >
           <NextIcon />
