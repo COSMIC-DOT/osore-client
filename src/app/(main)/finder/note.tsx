bff02600a9ac7bfa904f9437ea617b4c5b64cf36
kiuuon <k41471804@gmail.com>
2024-05-24 18:34:46 +0900

    feat: 노트 클릭 시 노트 페이지로 이동 구현

--- src/app/(main)/finder/note.tsx
+++ src/app/(main)/finder/note.tsx
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/navigation';
+
 function Note({ note }: { note: any }) {
+  const router = useRouter();
+
   return (
-    <div className="after: h-[315px] w-[435px] rounded-3xl border-[1px] border-black p-[15px]">
+    <div
+      className="after: h-[315px] w-[435px] rounded-3xl border-[1px] border-black p-[15px]"
+      onClick={() => router.push('/note')}
+      onKeyDown={() => router.push('/note')}
+      tabIndex={0}
+      role="button"
+    >
       <div>{note.title}</div>
       <div>{note.repository}</div>
       <div>{note.description}</div>
