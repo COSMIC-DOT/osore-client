fd5b99db500d8bc3950a7bc159dc713138328f3e
kiuuon <k41471804@gmail.com>
2024-05-27 12:37:51 +0900

    feat: 랜딩 페이지 레이아웃 틀 잡기 및 헤더 구현

---
+++ src/app/(landing)/header.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+function Header() {
+  return (
+    <header>
+      <div className="mx-auto flex h-[105px] w-[428px] items-center justify-between">
+        <Image className="h-[105px] w-[80px]" src="/images/icon.png" alt="아이콘" height={105} width={80} />
+        <Image className="h-[60px] w-[328px]" src="/images/logo.png" alt="로고" height={60} width={328} />
+      </div>
+      <div className="mt-[15px] text-center text-[28px]">로그인</div>
+    </header>
+  );
+}
+
+export default Header;
--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,21 @@
+import Header from './header';
+
 export default function Home() {
-  return <div className="w-2 bg-black">Hello world!</div>;
+  return (
+    <div className="flex">
+      <div className="h-[100vh] w-full bg-[#D9D9D9]" />
+      <div className="h-[100vh] min-w-[668px] bg-white">
+        <div className="ml-[20px] mt-[258px] h-[784px] w-[584px] px-[40px] py-[60px]">
+          <div className="flex h-[382px] w-[428px] flex-col justify-between">
+            <Header />
+            <div className="h-[140px] w-[400px]">
+              <button type="button">구글</button>
+              <button type="button">깃허브</button>
+            </div>
+          </div>
+          <div>이용약관 | 개인정보 보호 정책</div>
+        </div>
+      </div>
+    </div>
+  );
 }
