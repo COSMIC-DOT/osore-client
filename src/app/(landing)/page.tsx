0c14a270c587c1e3f0f553850069d65707efd4ea
kiuuon <k41471804@gmail.com>
2024-05-27 13:40:32 +0900

    feat: 구글 로그인 버튼 UI

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import GoogleLoginButton from './google-login-button';
 import Header from './header';
 
 export default function Home() {
@@ -5,11 +6,11 @@ export default function Home() {
     <div className="flex">
       <div className="h-[100vh] w-full bg-[#D9D9D9]" />
       <div className="h-[100vh] min-w-[668px] bg-white">
-        <div className="ml-[20px] mt-[258px] h-[784px] w-[584px] px-[40px] py-[60px]">
-          <div className="flex h-[382px] w-[428px] flex-col justify-between">
+        <div className="ml-[20px] mt-[258px] flex h-[784px] w-[584px] flex-col items-center justify-between px-[40px] py-[60px]">
+          <div className="flex h-[382px] w-[428px] flex-col items-center justify-between">
             <Header />
             <div className="h-[140px] w-[400px]">
-              <button type="button">구글</button>
+              <GoogleLoginButton />
               <button type="button">깃허브</button>
             </div>
           </div>
