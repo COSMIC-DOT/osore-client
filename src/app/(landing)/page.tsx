5af546ba7087ab589fcf9c248d865aeebb0ccba6
kiuuon <k41471804@gmail.com>
2024-05-27 13:47:38 +0900

    feat: 깃허브 로그인 버튼 UI

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,3 +1,4 @@
+import GitHubLoginButton from './github-login-button';
 import GoogleLoginButton from './google-login-button';
 import Header from './header';
 
@@ -9,12 +10,12 @@ export default function Home() {
         <div className="ml-[20px] mt-[258px] flex h-[784px] w-[584px] flex-col items-center justify-between px-[40px] py-[60px]">
           <div className="flex h-[382px] w-[428px] flex-col items-center justify-between">
             <Header />
-            <div className="h-[140px] w-[400px]">
+            <div className="flex h-[140px] w-[400px] flex-col justify-between">
               <GoogleLoginButton />
-              <button type="button">깃허브</button>
+              <GitHubLoginButton />
             </div>
           </div>
-          <div>이용약관 | 개인정보 보호 정책</div>
+          <div>이용약관 &nbsp; | &nbsp; 개인정보 보호 정책</div>
         </div>
       </div>
     </div>
