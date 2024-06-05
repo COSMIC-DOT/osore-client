df4be4e82b95919563bc37adc41673fef98caf4a
kiuuon <k41471804@gmail.com>
2024-06-05 14:27:37 +0900

    style: finder 페이지 레이아웃 수정

--- src/app/(main)/finder/page.tsx
+++ src/app/(main)/finder/page.tsx
@@ -5,7 +5,7 @@ import NoteList from './note-list';
 
 export default function Main() {
   return (
-    <div>
+    <div className="bg-[#F7F8F9] pt-[40px]">
       <Navigation />
       <Suspense fallback={<div>로딩중...</div>}>
         <NoteList />
