3b0d447ef9bd2dcffc261bf70784e54d667bce93
kiuuon <k41471804@gmail.com>
2024-07-23 17:08:58 +0900

    style: 기본 color 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    colors: {
+      black: '#1E1E1E',
+      white: '#FFFFFF',
+      gray0: '#F7F8F9',
+      gray1: '#E9EBEF',
+      gray2: '#C5C6CD',
+      gray3: '#999BA5',
+      gray4: '#6F717C',
+      primary: '#4E60FF',
+      primary_light: '#C3C9FF',
+      primary_dark: '#3617B3',
+      secondary: '#00E2D9',
+      secondary_light: '#A8F1EE',
+      secondary_dark: '#00B0A9',
+      error: 'FF5F4A',
+    },
     extend: {},
   },
   plugins: [],
