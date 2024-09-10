edd43f002a6a1b61a5910e831d06fb52a78bf382
kiuuon <k41471804@gmail.com>
2024-09-10 14:15:43 +0900

    feat: 코드 보여주는 부분 레이아웃 구성

---
+++ src/app/(main)/note/(main)/code-screen.tsx
@@ -0,0 +1,10 @@
+function CodeScreen() {
+  return (
+    <div className="mt-[29px] flex gap-[20px] px-[80px]">
+      <div className="h-[684px] w-[285px] border" />
+      <div className="h-[696px] w-full bg-[#D9D9D9]" />
+    </div>
+  );
+}
+
+export default CodeScreen;
--- src/app/(main)/note/page.tsx
+++ src/app/(main)/note/page.tsx
@@ -1,9 +1,11 @@
+import CodeScreen from './(main)/code-screen';
 import Navigation from './(navigation)/navigation';
 
 export default function Note() {
   return (
     <div>
       <Navigation />
+      <CodeScreen />
     </div>
   );
 }
