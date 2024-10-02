77288a6fbd3441379f9d7c21ec666db2c149f81d
kiuuon <k41471804@gmail.com>
2024-10-02 19:59:40 +0900

    feat: google oauth 로그인 구현

--- src/app/(landing)/google-login-button.tsx
+++ src/app/(landing)/google-login-button.tsx
@@ -1,10 +1,17 @@
+'use client';
+
 import GoogleIcon from '@/icons/google-icon';
 
 function GoogleLoginButton() {
+  const login = () => {
+    window.location.assign(`${process.env.NEXT_PUBLIC_BASE_URL}/api/auth/google`);
+  };
+
   return (
     <button
       type="button"
       className="text-body1 flex h-[60px] w-[400px] items-center justify-center gap-[20px] rounded-[24px] bg-gray0 shadow-[0_0_30px_rgba(0,0,0,0.05)] hover:bg-primary_light active:bg-primary active:text-white"
+      onClick={login}
     >
       <GoogleIcon />
       Continue with Google
