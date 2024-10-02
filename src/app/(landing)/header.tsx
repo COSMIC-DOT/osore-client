e3b7196a4c97b120648cbb6a5466b07834ff5938
Kiwon Kim <kiuuon29@gmail.com>
2024-10-02 19:33:50 +0900

    style: 랜딩 페이지 UI 수정 (#45)
    * style: 랜딩페이지 배경 애니메이션
    
    * style: 랜딩페이지 UI 수정
    
    * style: 랜딩페이지의 로그인 버튼 UI 수정

--- src/app/(landing)/header.tsx
+++ src/app/(landing)/header.tsx
@@ -2,12 +2,9 @@ import Image from 'next/image';
 
 function Header() {
   return (
-    <header>
-      <div className="flex h-[79px] w-[342px] items-center justify-between">
-        <Image className="h-[79px] w-[60px]" src="/images/icon.png" alt="아이콘" height={78} width={60} />
-        <Image className="h-[48px] w-[262px]" src="/images/logo.png" alt="로고" height={48} width={262} />
-      </div>
-      <div className="text-title2 mt-[12px] text-center">로그인</div>
+    <header className="flex h-[102px] w-[262px] flex-col gap-[12px]">
+      <Image className="h-[48px] w-[262px]" src="/images/logo.png" alt="로고" height={48} width={262} />
+      <div className="text-title2 text-center">로그인</div>
     </header>
   );
 }
