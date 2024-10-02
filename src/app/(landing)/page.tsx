0c4abd0a8172562d4aaa0736a0888876f3f5d4f6
kiuuon <k41471804@gmail.com>
2024-10-02 18:57:58 +0900

    style: 랜딩페이지 배경 애니메이션

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -1,13 +1,18 @@
+import Image from 'next/image';
+
 import GitHubLoginButton from './github-login-button';
 import GoogleLoginButton from './google-login-button';
 import Header from './header';
 
 export default function Home() {
   return (
-    <div className="flex">
-      <div className="h-[100vh] w-full bg-[#D9D9D9]" />
+    <div className="flex justify-end">
+      <Image className="bg1" src="/images/bg1.png" alt="배경1" height={1280} width={1440} />
+      <Image className="bg2" src="/images/bg2.png" alt="배경2" height={1280} width={1440} />
+      <Image className="bg3" src="/images/bg3.png" alt="배경3" height={1280} width={1440} />
+      <Image className="bg4" src="/images/bg4.png" alt="배경4" height={1280} width={1440} />
 
-      <div className="flex h-[100vh] min-w-[477px] flex-col items-center justify-end gap-[260px] bg-[#E9E9E9] px-[40px] py-[60px]">
+      <div className="flex h-[100vh] min-w-[477px] flex-col items-center justify-end gap-[260px] bg-black bg-opacity-10 px-[40px] py-[60px]">
         <div className="flex h-[353px] w-[400px] flex-col items-center justify-between">
           <Header />
           <div className="flex h-[140px] w-[400px] flex-col justify-between">
