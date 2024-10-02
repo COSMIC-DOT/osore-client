327ce9fe2517807344aa0f1654ceb49f89dd770b
kiuuon <k41471804@gmail.com>
2024-10-02 19:28:29 +0900

    style: 랜딩페이지의 로그인 버튼 UI 수정

--- src/app/(landing)/github-login-button.tsx
+++ src/app/(landing)/github-login-button.tsx
@@ -10,7 +10,7 @@ function GitHubLoginButton() {
   return (
     <button
       type="button"
-      className="text-body1 flex h-[60px] w-[400px] items-center justify-center gap-[20px] rounded-[24px] bg-white"
+      className="text-body1 flex h-[60px] w-[400px] items-center justify-center gap-[20px] rounded-[24px] bg-gray0 shadow-[0_0_30px_rgba(0,0,0,0.05)] hover:bg-primary_light active:bg-primary active:text-white"
       onClick={login}
     >
       <GitHubIcon />
