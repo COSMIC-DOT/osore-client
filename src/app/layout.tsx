a90b4c9c25fa59d4a6ea2cd4178783978e90ee00
kiuuon <k41471804@gmail.com>
2024-10-04 21:50:55 +0900

    feat: 오소리 파비콘 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: '오소리',
+  icons: {
+    icon: '/images/favicon.png',
+  },
 };
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
